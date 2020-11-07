b55744dfe409a02b751a4beb8a41f36cd330c255
John Khoo <john_khoo@u.nus.edu>
2020-11-07 15:31:41 +0800

    Add OS code to tMotor

--- main.c
+++ main.c
@@ -73,46 +73,58 @@ void tBrain(void *argument) {
 void tMotor(void *argument) {
 	UCHAR data;
 	for (;;) {
-		osMessageQueueGet(moveq, &data, NULL, MOVEDUR);
-		switch(data) {
-			case 1: // Forward
-				TPM0_C0V = 0;
-				TPM0_C1V = FREQUENCY_TO_MOD(50); // smaller mod value gives larger overall value means faster
-				TPM0_C2V = 0;
-				TPM0_C3V = FREQUENCY_TO_MOD(50);
-				break;
-			case 2: // Forward + Left
-				TPM0_C0V = 0;
-				TPM0_C1V = FREQUENCY_TO_MOD(50);
-				TPM0_C2V = 0;
-				TPM0_C3V = FREQUENCY_TO_MOD(100);
-				break;
-			case 3: // Forward + Right
-				TPM0_C0V = 0;
-				TPM0_C1V = FREQUENCY_TO_MOD(100);
-				TPM0_C2V = 0;
-				TPM0_C3V = FREQUENCY_TO_MOD(50);
-				break;
-			case 4: // Backward
-				TPM0_C0V = FREQUENCY_TO_MOD(50);
-				TPM0_C1V = 0;
-				TPM0_C2V = FREQUENCY_TO_MOD(50);
-				TPM0_C3V = 0;
-				break;
-			case 5: // Backward + Left
-				TPM0_C0V = FREQUENCY_TO_MOD(50);
-				TPM0_C1V = 0;
-				TPM0_C2V = FREQUENCY_TO_MOD(100);
-				TPM0_C3V = 0;
-				break;
-			case 6: // Backward + Right
-				TPM0_C0V = FREQUENCY_TO_MOD(100);
-				TPM0_C1V = 0;
-				TPM0_C2V = FREQUENCY_TO_MOD(50);
-				TPM0_C3V = 0;
-				break;
-		}
+    if (osMessageQueueGet(moveq, &data, NULL, MOVEDUR) == osOK) {
+      osEventFlagsSet(flags, FLAG_MOVE);
+      switch(data) { // TODO reorganise
+        case 1: // Forward
+          TPM0_C0V = 0;
+          TPM0_C1V = FREQUENCY_TO_MOD(50); // smaller mod value gives larger overall value means faster
+          TPM0_C2V = 0;
+          TPM0_C3V = FREQUENCY_TO_MOD(50);
+          break;
+        case 2: // Forward + Left
+          TPM0_C0V = 0;
+          TPM0_C1V = FREQUENCY_TO_MOD(50);
+          TPM0_C2V = 0;
+          TPM0_C3V = FREQUENCY_TO_MOD(100);
+          break;
+        case 3: // Forward + Right
+          TPM0_C0V = 0;
+          TPM0_C1V = FREQUENCY_TO_MOD(100);
+          TPM0_C2V = 0;
+          TPM0_C3V = FREQUENCY_TO_MOD(50);
+          break;
+        case 4: // Backward
+          TPM0_C0V = FREQUENCY_TO_MOD(50);
+          TPM0_C1V = 0;
+          TPM0_C2V = FREQUENCY_TO_MOD(50);
+          TPM0_C3V = 0;
+          break;
+        case 5: // Backward + Left
+          TPM0_C0V = FREQUENCY_TO_MOD(50);
+          TPM0_C1V = 0;
+          TPM0_C2V = FREQUENCY_TO_MOD(100);
+          TPM0_C3V = 0;
+          break;
+        case 6: // Backward + Right
+          TPM0_C0V = FREQUENCY_TO_MOD(100);
+          TPM0_C1V = 0;
+          TPM0_C2V = FREQUENCY_TO_MOD(50);
+          TPM0_C3V = 0;
+          break;
+      }
+    } else { // no command received, stop
+      osEventFlagsSet(flags, FLAG_CLEAR);
+      TPM0_C0V = FREQUENCY_TO_MOD(100);
+      TPM0_C1V = 0;
+      TPM0_C2V = FREQUENCY_TO_MOD(50);
+      TPM0_C3V = 0;
+    }
 	}
+}
+
+void tEventAudio(void *argument) {
+	
 }
 
 void tAudio(void *argument) {

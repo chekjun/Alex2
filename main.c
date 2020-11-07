a33e7002532eb080f4a8baf8cb8a8a7c77d8893d
John Khoo <john_khoo@u.nus.edu>
2020-11-07 15:28:01 +0800

    Write tBrain code and add EventAudio

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include "driver.h"
 
+// UART settings
 #define Q_SIZE (16)
 #define BAUD_RATE 9600
+#define CMD_CONN 100
+#define CMD_END 200
+#define CMD_W 'W'
+#define CMD_A 'A'
+#define CMD_S 'S'
+#define CMD_D 'D'
+
+// event flags
+#define FLAG_CONN ((uint32_t) 0x00000001)
+#define FLAG_END  ((uint32_t) 0x00000010)
+#define FLAG_MOVE ((uint32_t) 0x00000100)
+
+// movement settings
+#define MOVE_DUR 200
 
 void tBrain(void *argument);
 void tMotor(void *argument);
 void tLED(void *argument);
+void tEventAudio(void *argument);
 void tAudio(void *argument);
 
 osEventFlagsId_t flags;
@@ -27,6 +43,7 @@ int main(void) {
 	  moveq = osMessageQueueNew(Q_SIZE, sizeof(UCHAR), NULL);
 
     osThreadNew(tBrain, NULL, NULL); // TODO give high prio
+    osThreadNew(tEventAudio, NULL, NULL); // TODO give high prio
     osThreadNew(tLED, NULL, NULL);
 	  osThreadNew(tAudio, NULL, NULL);
 	  osThreadNew(tMotor, NULL, NULL);
@@ -36,13 +53,27 @@ int main(void) {
 }
 
 void tBrain(void *argument) {
-	
+  UCHAR cmd;
+  for (;;) {
+    osMessageQueueGet(rxq, &cmd, NULL, osWaitForever);
+    switch(cmd) {
+      case CMD_CONN: 
+        osEventFlagsSet(flags, FLAG_CONN);
+        break;
+      case CMD_END:
+        osEventFlagsSet(flags, FLAG_END);
+        break;
+      default:
+        osMessageQueuePut(moveq, &cmd, 0, osWaitForever);
+        break;
+    }
+  }
 }
 
 void tMotor(void *argument) {
-	uint8_t data;
+	UCHAR data;
 	for (;;) {
-		osMessageQueueGet(rxq, &data, NULL, 0);
+		osMessageQueueGet(moveq, &data, NULL, MOVEDUR);
 		switch(data) {
 			case 1: // Forward
 				TPM0_C0V = 0;
